de3ee935275e3d5c3ca0fee5f306e42914da97cb
yaohwang <yaohwang@163.com>
2018-12-07 16:22:28 +0800

    add info in DQN_modified.py

--- contents/5_Deep_Q_Network/DQN_modified.py
+++ contents/5_Deep_Q_Network/DQN_modified.py
@@ -9,7 +9,7 @@ Using:
 Tensorflow: r1.2
 """
 
-import numpy as np
+import numpy      as np
 import tensorflow as tf
 
 np.random.seed(1)
@@ -18,6 +18,7 @@ tf.set_random_seed(1)
 
 # Deep Q Network off-policy
 class DeepQNetwork:
+
     def __init__(
             self,
             n_actions,
@@ -66,39 +67,41 @@ class DeepQNetwork:
         self.sess.run(tf.global_variables_initializer())
         self.cost_his = []
 
+
     def _build_net(self):
         # ------------------ all inputs ------------------------
-        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input State
-        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')  # input Next State
-        self.r = tf.placeholder(tf.float32, [None, ], name='r')  # input Reward
-        self.a = tf.placeholder(tf.int32, [None, ], name='a')  # input Action
+        self.s  = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input State
+        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_') # input Next State
+        self.r  = tf.placeholder(tf.float32, [None, ]               , name='r')  # input Reward
+        self.a  = tf.placeholder(tf.int32  , [None, ]               , name='a')  # input Action
 
-        w_initializer, b_initializer = tf.random_normal_initializer(0., 0.3), tf.constant_initializer(0.1)
+        w_initializer = tf.random_normal_initializer(0., 0.3)
+        b_initializer = tf.constant_initializer(0.1)
 
         # ------------------ build evaluate_net ------------------
         with tf.variable_scope('eval_net'):
-            e1 = tf.layers.dense(self.s, 20, tf.nn.relu, kernel_initializer=w_initializer,
-                                 bias_initializer=b_initializer, name='e1')
-            self.q_eval = tf.layers.dense(e1, self.n_actions, kernel_initializer=w_initializer,
-                                          bias_initializer=b_initializer, name='q')
+            e1          = tf.layers.dense(self.s, 20, tf.nn.relu, kernel_initializer=w_initializer, bias_initializer=b_initializer, name='e1')
+            self.q_eval = tf.layers.dense(e1    , self.n_actions, kernel_initializer=w_initializer, bias_initializer=b_initializer, name='q')
 
         # ------------------ build target_net ------------------
         with tf.variable_scope('target_net'):
-            t1 = tf.layers.dense(self.s_, 20, tf.nn.relu, kernel_initializer=w_initializer,
-                                 bias_initializer=b_initializer, name='t1')
-            self.q_next = tf.layers.dense(t1, self.n_actions, kernel_initializer=w_initializer,
-                                          bias_initializer=b_initializer, name='t2')
+            t1          = tf.layers.dense(self.s_, 20, tf.nn.relu, kernel_initializer=w_initializer, bias_initializer=b_initializer, name='t1')
+            self.q_next = tf.layers.dense(t1     , self.n_actions, kernel_initializer=w_initializer, bias_initializer=b_initializer, name='t2')
 
         with tf.variable_scope('q_target'):
-            q_target = self.r + self.gamma * tf.reduce_max(self.q_next, axis=1, name='Qmax_s_')    # shape=(None, )
+            q_target      = self.r + self.gamma * tf.reduce_max(self.q_next, axis=1, name='Qmax_s_')    # shape=(None, )
             self.q_target = tf.stop_gradient(q_target)
+
         with tf.variable_scope('q_eval'):
             a_indices = tf.stack([tf.range(tf.shape(self.a)[0], dtype=tf.int32), self.a], axis=1)
             self.q_eval_wrt_a = tf.gather_nd(params=self.q_eval, indices=a_indices)    # shape=(None, )
+
         with tf.variable_scope('loss'):
             self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval_wrt_a, name='TD_error'))
+
         with tf.variable_scope('train'):
             self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+
 
     def store_transition(self, s, a, r, s_):
         if not hasattr(self, 'memory_counter'):
